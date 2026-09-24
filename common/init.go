7698d6ad73fafae6575c585d488cbb26f017f22c
Ubuntu <ubuntu@localhost.localdomain>
2026-09-24 17:30:47 +0800

    chore: default email verification to false, enable Turnstile bot protection

--- common/init.go
+++ common/init.go
@@ -136,7 +136,7 @@ func InitEnv() {
 
 	RegisterIPDailyLimit = GetEnvOrDefault("REGISTER_IP_DAILY_LIMIT", 5)
 	RegisterIPShortLimit = GetEnvOrDefault("REGISTER_IP_SHORT_LIMIT", 2)
-	EmailVerificationEnabled = GetEnvOrDefaultBool("EMAIL_VERIFICATION_ENABLED", EmailVerificationEnabled)
+	EmailVerificationEnabled = GetEnvOrDefaultBool("EMAIL_VERIFICATION_ENABLED", false)
 	initConstantEnv()
 }
 
